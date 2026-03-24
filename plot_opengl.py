926018c9f5c90328bd785a73981ce01666ce55c5
martin <mcbride.martin@gmail.com>
2026-03-24 15:29:24 +0000

    Add axes divisions markers and numbers

--- plot_opengl.py
+++ plot_opengl.py
@@ -82,7 +82,7 @@ class Axes:
     extent: tuple = (1, 1, 1) # (x, y, z) extent of axes in user space
     size: tuple = (1, 1, 1) # (x, y, z) size of axes in device space
     divs: tuple = (0.2, 0.2, 0.2) # (x, y, z) divisions in use space
-    text_offset: tuple = ((0.03, 0.06, 0), (0.12, 0.03, 0), (0, 0, 0))
+    text_offset: tuple = ((0.03, 0.06, 0), (0.12, 0.03, 0), (0.03, 0.06, 0))
     colors: tuple = ((1, 0, 0), (0, 1, 0), (0, 0, 1))
     div_colors: tuple = ((1, 0.5, 0.5), (0.5, 1, 0.5), (0.5, 0.5, 1))
     line_width: float = 4
@@ -142,14 +142,14 @@ class Axes:
         glColor3f(*self.colors[1])
         markers = self._get_divs(self.start[1], self.extent[1], self.divs[1])
         for m in markers:
-            pos = self.transform_from_graph((self.extent[0], m, 0))
+            pos = self.transform_from_graph((self.start[0]+self.extent[0], m, 0))
             glVertex3f(pos[0], pos[1], 0)
             glVertex3f(pos[0]+tick, pos[1], 0)
 
         glColor3f(*self.colors[2])
         markers = self._get_divs(self.start[2], self.extent[2], self.divs[2])
         for m in markers:
-            pos = self.transform_from_graph((0, self.extent[1], m))
+            pos = self.transform_from_graph((0, self.start[1]+self.extent[1], m))
             glVertex3f(0, pos[1], pos[2])
             glVertex3f(0, pos[1]+tick, pos[2])
 
@@ -160,8 +160,8 @@ class Axes:
         glColor3f(*self.colors[0])
         markers = self._get_divs(self.start[0], self.extent[0], self.divs[0])
         for m in markers[1:-1]:
-            pos = self.transform_from_graph((m, self.extent[1], 0))
-            glRasterPos3f(pos[0] + self.text_offset[0][0], self.start[1]+pos[1] + self.text_offset[1][0], 0)
+            pos = self.transform_from_graph((m, self.start[1]+self.extent[1], 0))
+            glRasterPos3f(pos[0] + self.text_offset[0][0], pos[1] + self.text_offset[0][1], 0)
             label = f"{m:.1f}"
             for c in label:
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
@@ -169,7 +169,7 @@ class Axes:
         glColor3f(*self.colors[1])
         markers = self._get_divs(self.start[1], self.extent[1], self.divs[1])
         for m in markers[1:-1]:
-            pos = self.transform_from_graph((self.extent[0], m, 0))
+            pos = self.transform_from_graph((self.start[0]+self.extent[0], m, 0))
             glRasterPos3f(pos[0] + self.text_offset[1][0], pos[1] + self.text_offset[1][1], 0)
             label = f"{m:.1f}"
             for c in label:
@@ -178,9 +178,8 @@ class Axes:
         glColor3f(*self.colors[2])
         markers = self._get_divs(self.start[2], self.extent[2], self.divs[2])
         for m in markers[1:-1]:
-            pos = self.transform_from_graph((0, self.extent[1], m))
-            print(pos, 0, pos[1] + self.text_offset[1][0],  pos[2] + self.text_offset[1][1])
-            glRasterPos3f(0, pos[1] + self.text_offset[1][1],  pos[2] + self.text_offset[1][2])
+            pos = self.transform_from_graph((0, self.start[1]+self.extent[1], m))
+            glRasterPos3f(0, pos[1] + self.text_offset[2][1],  pos[2] + self.text_offset[2][2])
             label = f"{m:.1f}"
             for c in label:
                 glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, ord(c))
